095d7555d569c4e58407786b6db6cc6426be8d10
prashan07 <60672026+prashan07@users.noreply.github.com>
2020-04-13 20:54:12 -0400

    Add files via upload

---
+++ all_arrays.php
@@ -0,0 +1,22 @@
+
+
+<?php
+$sale = array();
+$sale[0] = array("image" => "book_day.jpg", "title" => "World Book Day Sale", "desc" => "On the occasion of World Book Day, you can get 50% off the entire purchase.", "date" => "April 23rd");
+
+$sale[1] = array("image" => "math.jpg", "title" => "World Maths Day Sale", "desc" => "For all the mathematics enthusiast, we'll be giving away our collection of used maths books for as low as $5.", "date" => "March 4th");
+
+$sale[2] = array("image" => "reward.jpg", "title" => "$50 Reward", "desc" => "When you buy 10 books from our store, we'll reward you with $50 store credit. You can use the credit to buy any book of any genre from our store.", "date" => "");
+
+
+$features = array();
+
+$features[0] = array("icon" => "fab fa-paypal", "title" =>"We Accept Paypal and Paypal Credit", "desc" =>"No Interest charged for products paid in full in 6 months on purchases of $99+ with PayPal Credit. Subject to credit approval.");
+
+$features[1] = array("icon" => "fas fa-book", "title" =>"Rent a Book", "desc" =>"Rent and get the same book you want for less than half the new textbook price.");
+
+$features[2] = array("icon" => "fas fa-star", "title" =>"Just What You Needed", "desc" =>"We've got you covered for all of your preferences, whether used or digital books.");
+
+
+
+?>
---
+++ sale.php
@@ -0,0 +1,113 @@
+<?php
+  include 'all_arrays.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<title>WMU Bookstore</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/grid.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <header class ="header-section">
+        <nav class="navbar navbar-expand-lg navbar-custom">
+        
+        <a class="navbar-brand" href="index.html"><img src="img/book.jpg" alt="WMU Bookstore Logo"></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="Prasun1.html">Novels<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Prasun2.html">Biographies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sell.html">Sell Your Book</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sale.php">Sale</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="hours.html">Store Hours</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </header>
+
+<section class="sale_main">
+  <h2> Sales and Offers </h2>
+  
+  <?php
+    foreach ($sale as $key => $value) {
+
+      echo '<article class= "sale_offers">';
+      echo '<div class="jumbotron">';
+      echo '<div class="row">';
+      echo '<div class="span-1-of-2">';
+      echo '<figure>';
+      echo '<img src="img/'.$sale[$key]["image"].'">';
+      echo '</figure>';
+      echo '</div>';
+      echo '<div class="span-1-of-2">';
+      echo '<div class="textbox">';
+      echo '<h3><b>'.$sale[$key]["title"].'</b></h3>';
+      echo '<h4>'.$sale[$key]["date"].'</h4>';
+      echo '<p>'.$sale[$key]["desc"].'</p>';
+      echo '</div>';
+      echo '</div>';
+      echo '</div>';
+      echo '</div>';
+      echo '</article>';
+
+?>
+
+</section>
+
+<footer>
+   <nav class="navbar navbar-inverse">
+  <ul class="nav navbar-nav">
+    <li><a href="sale.html">Back to top</a></li>
+  </ul>
+</nav>
+
+
+<div class="container">
+  <a href="index.html" class="site-logo">
+          <img src="img/book.jpg" alt="WMU Bookstore Logo">
+        </a>
+
+  <p>WMU Bookstore &copy; 2020</p>
+
+
+</div>
+</footer>
+
+
+</body>
+</html>
