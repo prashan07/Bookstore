8c360e2211255173928f611393afaeb22720e8bb
Simar <heymanshudeepsingh@yahoo.com>
2020-04-15 20:41:02 -0400

    js

--- javascript/final.js
+++ javascript/final.js
@@ -1,27 +1,45 @@
+var total = 0;
+var items=0;
+var msg="";
 
-window.onload = function(){
-    document.getElementById("thumbnails").addEventListener("click", function(e){
+
+   function price(bookprice)
+   {
+    items++;
+    total+=bookprice;
 
 
-    var image_title = event.target.title;
-    var fig_caption = document.getElementById("featured").getElementsByTagName("figcaption")[0];
-    var new_source = old_source.replace("small", "medium");
-    var image = document.getElementById("featured").getElementsByTagName("img")[0];
-    fig_caption.innerHTML = image_title;
-    fig_caption.style.transition = "1s";
-    image.src = new_source;
-    image.title = image_title;
-    document.getElementById("featured").addEventListener("mouseover", hover);
-    document.getElementById("featured").addEventListener("mouseout", fadeover);
+   }
+const init=function(e){
 
+    let btn = document.querySelector("#cart");
+    btn.addEventListener('click',function()
+    {
+        window.document.location ='./prasun3.html';
+        localStorage.setItem('value',total)
     });
 
-    function hover(event){
-        document.getElementById("featured").getElementsByTagName("figcaption")[0].style.opacity = 0.8;
+};
 
-    }
+document.addEventListener('DOMContentLoaded',function(){
+    init();
+})
 
-    function fadeover(event){
-        document.getElementById("featured").getElementsByTagName("figcaption")[0].style.opacity = 0;
-    }
-}
+   function displayoutput()
+   {
+    msg = "<table><tr><th>Checkout</th></tr>"
+    msg+= "<tr><td>"
+    msg+= "Total Books:"
+    msg+= "</td><td>"
+    msg+= items;
+    msg+= "</td></tr>"
+    msg+= "<tr><td>"
+    msg+= "Total :"
+    msg+= "</td><td>"
+    msg+= toTimeString();
+    msg+= "</td></tr>"
+    msg+= "</table>"
+    document.getElementById("outputdiv").innerhtml=msg;
+
+   }
+
